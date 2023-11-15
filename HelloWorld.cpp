31a7e450bf2295b77647176b31f4775e637b0b6d
Thanh Trong <ngthtrong0354@gmail.com>
2023-11-16 01:14:03 +0700

    first commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout << "dap an nhu de bai yeu cau.";  
+    return 0;
+}
